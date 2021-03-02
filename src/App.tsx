016f800b0310668644cbccc976608dced1e7a1be
abid elhaj <xanaghost@gmail.com>
2021-03-02 22:48:38 +0100

    integrate the contact component into the App landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import ContactCard from "./components/ContactCard";
 
 function App() {
+  // test user
+  const john = {
+    userId: 12,
+    fullName: "Gina Snelly",
+    profileImageUrl: "https://randomuser.me/api/portraits/women/55.jpg",
+    trust: 75
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        Welcome !
-      </header>
+      <header className="App-header">Welcome !</header>
+
+      <div className="container">
+        <ContactCard user={john}></ContactCard>
+      </div>
     </div>
   );
 }
